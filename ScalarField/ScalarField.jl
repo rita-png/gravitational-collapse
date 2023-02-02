628929693046b5d81b711be2f7da077f28c1d850
Rita Santos <80919485+rita-png@users.noreply.github.com>
2023-02-02 12:50:40 +0000

    scalar field

--- ScalarField/ScalarField.jl
+++ ScalarField/ScalarField.jl
@@ -180,12 +180,16 @@ int(x) = floor(Int, x)
 SFconstraint_psibar(psibar0,R1)=-sin.(R1)
 
 # Test Model RHSs for the bulk equations.
-SFconstraint_m(m0,R1,time)=2*pi .* R1 .^2 *(1 .- 2 .* (1 .- R1) ./ R1 .* state_array[int.(R1 ./ dx .+ 1),1]) #* ((1 .- R1) ./ R1 .* state_array[int.(R1./dx.+1),4] .- state_array[int.(R1./dx.+1),3] ./ R1^2)^2
+# (3.6.16)
+#SFconstraint_m(m0,R1,time)=2*pi .* R1 .^2 *(1 .- 2 .* (1 .- R1) ./ R1 .* state_array[int.(R1 ./ dx .+ 1),1]) * ((1 .- R1) ./ R1 .* state_array[int.(R1./dx.+1),4] .- state_array[int.(R1./dx.+1),3] ./ R1^2)^2
 
-SFconstraint_beta(beta0,R1,time)=2*pi .* R1 .* (1 .- R1) #* ((1 .- R1) ./ R1 .* state_array[int.(R1./dx.+1),4] .- state_array[int.(R1./dx.+1),3] ./ R1^2)^2
+SFconstraint_m(beta0,R1,time)=2*pi ./ R1 .^2 .* (1-2*(1 .- R1) .* state_array[int.(R1 ./ dx .+ 1),1] ./ R1) .* (state_array[int.(R1 ./ dx .+ 1),3] .+ (R1 .- 1) R1 .* state_array[int.(R1 ./ dx .+ 1),4]) .^2
+
+#SFconstraint_beta(beta0,R1,time)=2*pi .* R1 .* (1 .- R1) * ((1 .- R1) ./ R1 .* state_array[int.(R1./dx.+1),4] .- state_array[int.(R1./dx.+1),3] ./ R1^2)^2
+SFconstraint_beta(m0,R1,time)=2*pi ./ R1 .^3 .*(R1 .+ 2 .* (R1 .- 1) .* state_array[int.(R1 ./ dx .+ 1),1] ) .* (state_array[int.(R1 ./ dx .+ 1),3] .+ (R1 .- 1) .* R1 .* state_array[int.(R1 ./ dx .+ 1),4]) .^2
 
 
-"function globalwork()
+"function globalfunc()
     return  R1 .* state_array[4:L-3,4]
 end"
 
