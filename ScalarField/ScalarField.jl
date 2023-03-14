e0c5a6de345c4bbaa00a4189dba2846c790a256c
Rita Santos <80919485+rita-png@users.noreply.github.com>
2023-03-14 18:56:39 +0000

    implementing midstep on molfunction

--- ScalarField/ScalarField.jl
+++ ScalarField/ScalarField.jl
@@ -3,7 +3,7 @@
 function init_gaussian(x,r0,sigma,A)
     n=length(x);
     if n==1
-        z= A * (x/(1-x))^2 * exp(-((x/(1-x)-r0)/sigma)^2)
+        z= A * (x/(1-x))^3.0 * exp(-((x/(1-x)-r0)/sigma)^2)
     else
         z=zeros(n);
         for i in 1:n
@@ -20,12 +20,14 @@ end
 function init_gaussian_der(x,r0,sigma,A)
     n=length(x);
     if n==1
-        z= 2*A * x/(1-x)^3 * exp(-((x/(1-x)-r0)/sigma)^2) * (1 - x/(1-x) * (x/(1-x) - r0) / sigma^2)
+        #z= 2*A * x/(1-x)^3 * exp(-((x/(1-x)-r0)/sigma)^2) * (1 - x/(1-x) * (x/(1-x) - r0) / sigma^2)
+        z= A * exp(-((x/(1-x)-r0)/sigma)^2) * (3 * x^2 / (1-x) ^4 - (x/(1-x))^3 * (2*(x-r0*(-x+1)))/(sigma^2*(1-x)^3))
     else
         z=zeros(n);
         for i in 1:n
             if i<n-4 #avoid NaN for x=1, otherwise, it's 0
-                z[i] = 2*A * x[i]/(1-x[i])^3 * exp(-((x[i]/(1-x[i])-r0)/sigma)^2) * (1 - x[i]/(1-x[i]) * (x[i]/(1-x[i]) - r0) / sigma^2)
+                #z[i] = 2*A * x[i]/(1-x[i])^3 * exp(-((x[i]/(1-x[i])-r0)/sigma)^2) * (1 - x[i]/(1-x[i]) * (x[i]/(1-x[i]) - r0) / sigma^2)
+                z[i]=A * exp(-((x[i]/(1-x[i])-r0)/sigma)^2) * (3 * x[i]^2 / (1-x[i]) ^4 - (x[i]/(1-x[i]))^3 * (2*(x[i]-r0*(-x[i]+1)))/(sigma^2*(1-x[i])^3))
             end
         end
     end
@@ -52,13 +54,17 @@ function extrapolate_in(y0,y1,y2,y3)
 end
 
 # Updating Grid
-function update_grid(data,dx,T,k,spl_funcs)
+function update_grid(data,T,k)
     
-    #evolve grid
-    data=rungekutta4molstep(Grid_RHS,data,T,k,0,spl_funcs) #evolve X
-    data=ghost(data)
+    dx = data[2,5]-data[1,5]
 
+    #evolve grid
+    data = rungekutta4molstep(Grid_RHS,data,T,k,0,data[:,5]) #evolve X
+    #data=ghost(data)
+    
     X = data[:,5]
+
+    
 
     return X
 """
@@ -96,17 +102,20 @@ end
 
 # Runge Kutta integrator used for the method of lines
 
-function rungekutta4molstep(f,y00,T,w::Int64,ex,spl_func,X)
+function rungekutta4molstep(f,y00,T,w::Int64,ex,X)
     y = y00;
-        h = dt; # only for equally spaced grid in time and space, otherwise (T[w+1] - T[w])
+    X=collect(X)
+        h = T[w+1]-T[w]
+        #print("\n\nh = ", h, " \n")
+        #h = dt; # only for equally spaced grid in time and space, otherwise (T[w+1] - T[w])
         #println("y[:,4] ", y[:,4])
         #println("############")
-        k1 = f(y[:,:], T[w],spl_func,X)
+        k1 = f(y[:,:], T[w],X)
         k1=ghost(k1)
         #println("k1[:,4] ", k1[:,4])
         ####println("y[:,4] ", y[:,4])
         #println("############")
-        k2 = f(y[:,:] .+ k1 .* h/2, T[w] .+ h/2,spl_func,X)
+        k2 = f(y[:,:] .+ k1 .* h/2, T[w] + h/2,X)
         k2=ghost(k2)
         #println("k2[:,4] ", k2[:,4])
         ####println("y[:,4] .+ k1 .* h/2 ", y[:,4] .+ k1 .* h/2)
@@ -115,20 +124,25 @@ function rungekutta4molstep(f,y00,T,w::Int64,ex,spl_func,X)
         #println("k2[:,4].-k1[:,4] ", k2[:,4] .- k1[:,4])
         ####println("y[:,4] .+ k1 .* h/2 .- y[:,4]", y[:,4] .+ k1 .* h/2 .- y[:,4])
         #println("############")
-        k3 = f(y[:,:] .+ k2 .* h/2, T[w] .+ h/2,spl_func,X)
+        k3 = f(y[:,:] .+ k2 .* h/2, T[w] + h/2,X)
         k3=ghost(k3)
 
         #println("############")
-        k4 = f(y[:,:] .+ k3 .* h, T[w] .+ h,spl_func,X)
+        k4 = f(y[:,:] .+ k3 .* h, T[w] + h,X)
         k4=ghost(k4)
 
+        """print("\n\ny[50,4] .+ k3 .* h\n\n",y[50,4] .+ k3[50,4] .* h)
+        print("\n\ny[50,4]\n\n",y[50,4])
+        print("\n\nk3\n\n",k3[50,4])
+        print("\n\nh\n\n",h)
+"""
         #println("k4[:,4] .- k1[:,4] ", k4[:,4] .- k1[:,4])
         #println("############")
 
         #println(y[:,4]-.y[:,4] .- k1 .* h/2)
         y[:,:] = y[:,:] .+ (h/6) .* (k1 .+ 2 * k2 .+ 2 * k3 .+ k4)
         
-    return ghost(y[:,:])
+    return y[:,:]#ghost(y[:,:])
 end
 
 
@@ -154,14 +168,14 @@ function ghost(y)
     L=length(y[:,1])
     
     #inner boundary extrapolation
-    y[3,:]=extrapolate_in(y[4,:], y[5,:], y[6,:], y[7,:])
-    y[2,:]=extrapolate_in(y[3,:], y[4,:], y[5,:], y[6,:])
-    y[1,:]=extrapolate_in(y[2,:], y[3,:], y[4,:], y[5,:])
+    y[3,1:4]=extrapolate_in(y[4,1:4], y[5,1:4], y[6,1:4], y[7,1:4])
+    y[2,1:4]=extrapolate_in(y[3,1:4], y[4,1:4], y[5,1:4], y[6,1:4])
+    y[1,1:4]=extrapolate_in(y[2,1:4], y[3,1:4], y[4,1:4], y[5,1:4])
 
     #outer boundary extrapolation
-    y[L-2,:]=extrapolate_out(y[L-6,:], y[L-5,:], y[L-4,:], y[L-3,:])
-    y[L-1,:]=extrapolate_out(y[L-5,:], y[L-4,:], y[L-3,:], y[L-2,:])
-    y[L,:]=extrapolate_out(y[L-4,:], y[L-3,:], y[L-2,:], y[L-1,:])
+    y[L-2,1:4]=extrapolate_out(y[L-6,1:4], y[L-5,1:4], y[L-4,1:4], y[L-3,1:4])
+    y[L-1,1:4]=extrapolate_out(y[L-5,1:4], y[L-4,1:4], y[L-3,1:4], y[L-2,1:4])
+    y[L,1:4]=extrapolate_out(y[L-4,1:4], y[L-3,1:4], y[L-2,1:4], y[L-1,1:4])
    
 
     return y
@@ -194,6 +208,18 @@ end
 Der(y,i,k,X)=(y[i+3,k]-9*y[i+2,k]+45*y[i+1,k]-45*y[i-1,k]+9*y[i-2,k]-y[i-3,k])/(60*(X[i+1]-X[i])); #6th order
 DDer(y,i,k,X)=(y[i+3,k]/90-3*y[i+2,k]/20+3*y[i+1,k]/2-49*y[i,k]/18+3*y[i-1,k]/2-3*y[i-2,k]/20+y[i-3,k]/90)/((X[i+1]-X[i])^2); #6th order
 
+# Finite difference approximation
+"""function Der(y,i,k,X)
+
+    if i>6 && i<L-5 # central
+        z = wwgwr
+    elseif i<=6 # backward
+        z = wgwwg
+    else # forward
+        z = grweg
+    return
+
+"""
 function D(y,i,k,X)
 
     if i>=5 && i<=L-4
@@ -206,7 +232,7 @@ function D(y,i,k,X)
             aux = (-y[j+2,k]+8*y[j+1,k]-8*y[j-1,k]+y[j-2,k])/(12*(dx))
             interp_data = vcat(interp_data, aux)
         end
-        spl = scipyinterpolate.splrep(X[5:L-4], interp_data,k=4)
+        spl = scipyinterpolate.splrep(X[5:L-4], interp_data,k=2)
         Der_func(x) = scipyinterpolate.splev(x, spl)
         z = Der_func(X[i])[1]
         #println("Der!, i=",i)
@@ -226,7 +252,7 @@ function DD(y,i,k,X)
             aux = (-y[i+2,k]+16*y[i+1,k]-30*y[i,k]+16*y[i-1,k]-y[i-2,k])/(12*(dx)^2)
             interp_data = vcat(interp_data, aux)
         end
-        spl = scipyinterpolate.splrep(X[4:L-3], interp_data,k=4)
+        spl = scipyinterpolate.splrep(X[4:L-3], interp_data,k=2)
         DDer_func(xx) = scipyinterpolate.splev(xx, spl)
         z = DDer_func(X[i])[1]
         #println("DDer!, i=",i)
@@ -234,25 +260,7 @@ function DD(y,i,k,X)
     return z
 end
 
-function Der_cont(interp_func,x,i)
 
-    dxx = X[i+1]-X[i]
-
-    f=interp_func
-
-    return ((-f(x+2*dxx) + 8*f(x+dxx) - 8*f(x-dxx) + f(x-2*dxx)) / (12*dxx))
-
-end
-
-function DDer_cont(interp_func,x,i)
-
-    dxx = X[i+1]-X[i]
-
-    f=interp_func
-
-    return ((-f(x+2*dxx) + 16*f(x+dxx) - 30*f(x) + 16*f(x-dxx) - f(x-2*dxx)) / (12*dxx^2))
-
-end
 
 
 int(x) = floor(Int, x)
@@ -266,6 +274,8 @@ function SFconstraint_beta(beta0,x1,time,funcs)
     psi = funcs[1]
     derpsi = funcs[2]
     
+    #println("psi(x1)[1]", psi(x1)[1])
+
     if x1<10^(-15)
         z = 0.0
     elseif abs.(x1 .- 1.0)<10^(-15)
@@ -283,36 +293,31 @@ function SFconstraint_beta(beta0,x1,time,funcs)
     #return derpsi(x1)[1]
 end
 
+function new_constraint(f0,x1,time,funcs)
+    
+    z = sin(x1*20+time)
 
-function SFconstraint_m(m0,x1,u,interp_func,psiarray)
+    return z
+end
 
-    derpsi = interp_func
+
+function SFconstraint_m(m0,x1,time,funcs)
+
+    psi = funcs[1]
+    derpsi = funcs[2]
 
     if x1<10^(-15)
         z = 0
     elseif abs.(x1 .- 1.0)<10^(-15)
-        z = 2.0 .* pi .* (psiarray[k]) .^ 2.0
+        z = 2.0 .* pi .* (psi(x1)[1]) .^ 2.0
     else
         #z = 2.0 .* pi .* (x1 .+ 2.0 .* (x1 .- 1.0) .* m0) ./ x1 .^3.0 .* (psiarray[k] .+ (x1 .- 1.0) .* x1 .* derpsi(x1)) .^2.0
-        z = 2.0 .* pi .* (x1 .+ 2.0 .* (x1 .- 1.0)) ./ x1 .^3.0 .* ((x1 .- 1.0) .* x1 .* derpsi(x1)) .^ 2.0
+        z = 2.0 .* pi .* (x1 .+ 2.0 .* (x1 .- 1.0) .* m0) ./ x1 .^3.0 .* (psi(x1)[1] .+     (x1 .- 1.0) .* x1 .* derpsi(x1)[1]) .^ 2.0
     end
     
     return z
 end
 
-"""function SFconstraint_temp(m0,x1,u,interp_func,psiarray,k)
-
-    derpsi = interp_func
-
-    if x1<10^(-15)
-        z = 0
-    else
-        z = 2.0 .* pi .* (x1 .+ 2.0 .* (x1 .- 1.0)) ./ x1 .^3.0 .* ((x1 .- 1.0) .* x1 .* derpsi(x1)) .^ 2.0
-    end
-
-    #z=2.0 * pi
-    return z
-end"""
 
 
 function bulkSF(y,i,X)
@@ -320,7 +325,7 @@ function bulkSF(y,i,X)
     
     #psi,X
 
-    dy=-1.0/2.0*exp(2.0*y[i,2])*((2.0*exp(2.0*(X[i]-y[i,3]+X[i]*y[i,3])*y[i,2]/X[i])*(X[i]-1.0)^2*(X[i]*((X[i]-1.0)*Der(y,i,1,X)+X[i]*Der(y,i,2,X))+y[i,1]*(1.0+2.0*(X[i]-1.0)*X[i]*Der(y,i,2,X))))/X[i]^2 - ((-1.0+X[i])^3*(X[i]+2.0*(X[i]-1.0)*y[i,1])*y[i,4])/X[i]^2 - ((1.0-X[i])^3*(1.0-2.0*(X[i]-1.0)^2*Der(y,i,1,X))*y[i,4])/X[i] - (2.0*(X[i]-1.0)^4*(X[i]+2.0*(X[i]-1.0)*y[i,1])*Der(y,i,2,X)*y[i,4])/X[i] - (Der(y,i,4,X)) - (2.0*(X[i]-1.0)*y[i,1]*Der(y,i,4,X))/X[i]);# - dissipation6(y,i,0.01)[4];
+    dy=-1.0/2.0*exp(2.0*y[i,2])*((2.0*exp(2.0*(X[i]-y[i,3]+X[i]*y[i,3])*y[i,2]/X[i])*(X[i]-1.0)^2*(X[i]*((X[i]-1.0)*Der(y,i,1,X)+X[i]*Der(y,i,2,X))+y[i,1]*(1.0+2.0*(X[i]-1.0)*X[i]*Der(y,i,2,X))))/X[i]^2 - ((-1.0+X[i])^3*(X[i]+2.0*(X[i]-1.0)*y[i,1])*y[i,4])/X[i]^2 - ((1.0-X[i])^3*(1.0-2.0*(X[i]-1.0)^2*Der(y,i,1,X))*y[i,4])/X[i] - (2.0*(X[i]-1.0)^4*(X[i]+2.0*(X[i]-1.0)*y[i,1])*Der(y,i,2,X)*y[i,4])/X[i] - (Der(y,i,4,X)) - (2.0*(X[i]-1.0)*y[i,1]*Der(y,i,4,X))/X[i]) #- dissipation6(y,i,0.01)[4];
     #dy[4]=-1.0/2.0*exp(2.0*y[i,2]) .* ((2.0*exp(2.0 .* (X[i] .- y[i,3] .+ X[i] .* y[i,3]) .* y[i,2] ./ X[i]) .* (X[i] .- 1.0) .^ 2.0 .* (X[i] .* ((X[i] .- 1.0) .* Der(y,i,1,X) .+ X[i] .* Der(y,i,2,X)) .+ y[i,1] .* (1.0 .+ 2.0 .* (X[i] .- 1.0) .* X[i] .* Der(y,i,2,X)))) ./ X[i] .^ 2.0 .- ((-1.0 .+ X[i]) .^ 3.0 .* (X[i] .+ 2.0 .* (X[i] .- 1.0) .* y[i,1]) .* y[i,4]) ./ X[i] .^ 2.0 .- ((1.0 .- X[i]) .^ 3.0 .* (1.0 .- 2.0 .* (X[i] .- 1.0) .^ 2.0 .* Der(y,i,1,X)) .* y[i,4]) ./ X[i] .- (2.0 .* (X[i] .- 1.0) .^ 4.0 .* (X[i] .+ 2.0 .* (X[i] .- 1.0) .* y[i,1]) .* Der(y,i,2,X) .* y[i,4]) ./ X[i] .- (Der(y,i,4,X)) .- (2.0 .* (X[i] .- 1.0) .* y[i,1] .* Der(y,i,4,X)) ./ X[i]);# - dissipation6(y,i,0.01)[4];
 
 
@@ -384,93 +389,98 @@ end
 
 # Defining the function in the RHS of the evolution equation system
 
-function SF_RHS(data,t,interp_func,X)
+function SF_RHS(data,t,X)
     
     L=length(X)
     dy=zeros((L,length(data[1,:])));
 
-""" 
-    for i in 5:(L-4)
-        dy[i,4]=-1.0/2.0*exp(2.0*data[i,2])*((2.0*exp(2.0*(X[i]-data[i,3]+X[i]*data[i,3])*data[i,2]/X[i])*(X[i]-1.0)^2*(X[i]*((X[i]-1.0)*Der(data,i,1,X)+X[i]*Der(data,i,2,X))+data[i,1]*(1.0+2.0*(X[i]-1.0)*X[i]*Der(data,i,2,X))))/X[i]^2 - ((-1.0+X[i])^3*(X[i]+2.0*(X[i]-1.0)*data[i,1])*data[i,4])/X[i]^2 - ((1.0-X[i])^3*(1.0-2.0*(X[i]-1.0)^2*Der(data,i,1,X))*data[i,4])/X[i] - (2.0*(X[i]-1.0)^4*(X[i]+2.0*(X[i]-1.0)*data[i,1])*Der(data,i,2,X)*data[i,4])/X[i] - (Der(data,i,4,X)) - (2.0*(X[i]-1.0)*data[i,1]*Der(data,i,4,X))/X[i]);# - dissipation6(y,i,0.01)[4];
+    # update interpolation psi functions for constraint_beta here
+    spl_psi = scipyinterpolate.splrep(X[4:L-3], data[4:L-3,3],k=4)
+    psi_func(x) = scipyinterpolate.splev(x, spl_psi)
+
+    spl_derpsi = scipyinterpolate.splrep(X[4:L-3], data[4:L-3,4],k=4)
+    derpsi_func(x) = scipyinterpolate.splev(x, spl_derpsi)
+
+    funcs = [psi_func derpsi_func]
+
+    #rk4wrapper to update psi
+    psi0=0
+    SFconstraint_psi(psi0,x) = scipyinterpolate.splev(x, spl_derpsi)
+    data[4:L-3,3] = rungekutta4(SFconstraint_psi,psi0,X[4:L-3])
+    data = ghost(data)
+    global state_array[:,3] = data[:,3]
+
+    #rk4wrapper to update beta
+    beta0=0
+    data[4:L-3,2] = rk4wrapper(SFconstraint_beta,beta0,X[4:L-3],t,funcs)
+    data = ghost(data)
+    global state_array[:,2] = data[:,2]
+
+    #rk4wrapper to update m
+    m0=0
+    data[4:L-3,1]=rk4wrapper(SFconstraint_m,m0,X[4:L-3],t,funcs)
+    data = ghost(data)
+    global state_array[:,1] = data[:,1]
+
+
     
-        
-        #-1.0/2.0*exp(2.0*data[i,2]) * ((2.0*exp(2.0*(X[i]-data[i,3]+X[i]*data[i,3])*data[i,2]/X[i])*(X[i]-1.0)^2*(X[i]*((X[i]-1.0)*Der(data,i,1,X)+X[i]*Der(data,i,2,X))+data[i,1]*(1.0+2.0*(X[i]-1.0)*X[i]*Der(data,i,2,X))))/X[i]^2 ) ;
+    #globally?
+    #psi another RHS. but do it later
+
+    for i in 4:(L-3)
+        dy[i,4] = 1#data[i,2]
     end
-#- (Der(y,i,4,X)) - (2.0*(X[i]-1.0)*y[i,1]*Der(y,i,4,X))/X[i]
-    
+
+
+
+
 
 """
-for i in 1:(L)
-    dy[i,4] = 1.0
-end
-"""    for i in 4:(L-3)
+    #identify 1st positive gridpoint X[i]
+    origin_i = 4
+    for i in 1:L
+        if X[i] >= 0
+            origin_i = i
+            break
+        end
+    end
+    println("\nOrigin of the grid is at t = ", t, " is i = ", origin_i, "\n")
+   """
+"""
+    for i in 4:L-3 #ORI
         if X[i]<10^(-15) #left
-
-            #dy[i,1] to dy[i,3] stay 0
             dy[i,4]= 0.0 #-dissipation4(y,i,0.1)[4];
 
         elseif X[i] < (1-10^(-15)) #bulk
-            dy[i,4]=bulkSF(data,i,X) - dissipation4(data,i,0.1)[4];
+            dy[i,4]=bulkSF(data,i,X) - dissipation6(data,i,0.05)[4];
 
-            #bulkSF(data,i,X) .- dissipation4(data,i,0.1)[4];
         else #right
-            #dy[L-3,4]=1.0/2.0*exp(2.0*y[i,2])*Der(y,i,3,X) - dissipation4(y,i,0.1)[4]
-            dy[i,4]=bulkSF(data,i,X) - dissipation4(data,i,0.1)[4];
-            #dy[L-3,4]=-1.0/2.0*exp(2.0*y[i,2])*((2.0*exp(2.0*(X[i]-y[i,3]+X[i]*y[i,3])*y[i,2]/X[i])*(X[i]-1.0)^2*(X[i]*((X[i]-1.0)*Der(y,i,1,X)+X[i]*Der(y,i,2,X))+y[i,1]*(1.0+2.0*(X[i]-1.0)*X[i]*Der(y,i,2,X))))/X[i]^2 - ((-1.0+X[i])^3*(X[i]+2.0*(X[i]-1.0)*y[i,1])*y[i,4])/X[i]^2 - ((1.0-X[i])^3*(1.0-2.0*(X[i]-1.0)^2*Der(y,i,1,X))*y[i,4])/X[i] - (2.0*(X[i]-1.0)^4*(X[i]+2.0*(X[i]-1.0)*y[i,1])*Der(y,i,2,X)*y[i,4])/X[i] - (Der(y,i,4,X)) - (2.0*(X[i]-1.0)*y[i,1]*Der(y,i,4,X))/X[i]) - dissipation4(y,i,0.06)[4];
+            dy[i,4]=bulkSF(data,i,X) - dissipation6(data,i,0.05)[4];
         end
     end
     """
     
     
-"""    #outer boundary
-    dy[L-3,4]=extrapolate_out(dy[L-7,4], dy[L-6,4], dy[L-5,4], dy[L-4,4])#1.0/2.0*exp(2.0*y[L-3,2])* Der(y,L-3,4)
-    dy[L-2,4]=extrapolate_out(dy[L-6,4], dy[L-5,4], dy[L-4,4], dy[L-3,4])#1.0/2.0*exp(2.0*y[L-3,2])* Der(y,L-2,4)
-    dy[L-1,4]=extrapolate_out(dy[L-5,4], dy[L-4,4], dy[L-3,4], dy[L-2,4])
-    dy[L,4]=extrapolate_out(dy[L-4,4], dy[L-3,4], dy[L-2,4], dy[L-1,4])"""
-    
-"""    for i in 1:L
-        dy[i,4]=data[i,1]
-    end"""
+    #outer boundary
+    #dy[L-3,4]=extrapolate_out(dy[L-7,4], dy[L-6,4], dy[L-5,4], dy[L-4,4])#1.0/2.0*exp(2.0*y[L-3,2])* Der(y,L-3,4)
+    #dy[L-2,4]=extrapolate_out(dy[L-6,4], dy[L-5,4], dy[L-4,4], dy[L-3,4])#1.0/2.0*exp(2.0*y[L-3,2])* Der(y,L-2,4)
+    #dy[L-1,4]=extrapolate_out(dy[L-5,4], dy[L-4,4], dy[L-3,4], dy[L-2,4])
+    #dy[L,4]=extrapolate_out(dy[L-4,4], dy[L-3,4], dy[L-2,4], dy[L-1,4])
+  
 
     return dy
-    """derr=zeros(L)
-    for i in 4:L-3
-        derr[i] = Der(data,i,1,X)
-    end
-    return derr"""
-    #return data[:,1]
+
 end
 
-function GP_RHS(y,t,interp_func,X)
 
+function Grid_RHS(y,t,X)
+    
     L=length(X)
     dy=zeros((L,length(y[1,:])));
-
-    # updating ghostpoints
-    for i in 1:3
-        dy[i,4]= dy[i,4]=-1.0/2.0*exp(2.0*y[i,2])* ((2.0*exp(2.0*(X[i]-y[i,3]+X[i]*y[i,3])*y[i,2]/X[i])*(X[i]-1.0)^2*(X[i]*((X[i]-1.0)*Der(y,i,1,X)+X[i]*Der(y,i,2,X))+y[i,1]*(1.0+2.0*(X[i]-1.0)*X[i]*Der(y,i,2,X))))/X[i]^2 - ((-1.0+X[i])^3*(X[i]+2.0*(X[i]-1.0)*y[i,1])*y[i,4])/X[i]^2 - ((1.0-X[i])^3*(1.0-2.0*(X[i]-1.0)^2*Der(y,i,1,X))*y[i,4])/X[i] - (2.0*(X[i]-1.0)^4*(X[i]+2.0*(X[i]-1.0)*y[i,1])*Der(y,i,2,X)*y[i,4])/X[i] - (DDer(y,i,3,X)) - (2.0*(X[i]-1.0)*y[i,1]*DDer(y,i,3,X))/X[i]);# - dissipation6(y,i,0.01)[4];
-    end
-    for i in L-2:L
-        dy[i,4]= dy[i,4]=-1.0/2.0*exp(2.0*y[i,2])* ((2.0*exp(2.0*(X[i]-y[i,3]+X[i]*y[i,3])*y[i,2]/X[i])*(X[i]-1.0)^2*(X[i]*((X[i]-1.0)*Der(y,i,1,X)+X[i]*Der(y,i,2,X))+y[i,1]*(1.0+2.0*(X[i]-1.0)*X[i]*Der(y,i,2,X))))/X[i]^2 - ((-1.0+X[i])^3*(X[i]+2.0*(X[i]-1.0)*y[i,1])*y[i,4])/X[i]^2 - ((1.0-X[i])^3*(1.0-2.0*(X[i]-1.0)^2*Der(y,i,1,X))*y[i,4])/X[i] - (2.0*(X[i]-1.0)^4*(X[i]+2.0*(X[i]-1.0)*y[i,1])*Der(y,i,2,X)*y[i,4])/X[i] - (DDer(y,i,3,X)) - (2.0*(X[i]-1.0)*y[i,1]*DDer(y,i,3,X))/X[i]);# - dissipation6(y,i,0.01)[4];
-    end
-
-    return dy
-end
-
-function Grid_RHS(y,t,interp_funcs,X)
-    
-    m = interp_funcs[1]
-    beta = interp_funcs[2]
-
-    L=length(X)
-    dy=zeros((L,length(y[1,:])));
-
-    
 
     for i in 4:(L-3)
         if X[i]<10^(-7) #left
 
-            #dy[i,1] to dy[i,3] stay 0
             dy[i,5]=-1.0/2.0*exp(2.0*y[i,2]);
 
         else #bulk
