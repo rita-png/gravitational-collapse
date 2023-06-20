c11c240fc036fc25804ee201f4a9c797ef68919c
Rita <ritapsantos@tecnico.ulisboa.pt>
2023-06-20 19:05:09 +0100

    run oolon

--- ScalarField/Evolution_ScalarField.jl
+++ ScalarField/Evolution_ScalarField.jl
@@ -7,7 +7,7 @@ using Dierckx
 A=0.0492646484375
 run=1
 #global dir = "/home/rita13santos/Desktop/MSc Thesis/Git/ScalarField/DATA/bisectionsearch"
-global "/home/ritapsantos/data/ritapsantos"#dir = ENV["gc_datapath"]
+global dir = "/home/ritapsantos/data/ritapsantos"#dir = ENV["gc_datapath"]
 
 #println(dir)
 global bisection = true
