fec45050de6eb0420a1dbf29cc27f3af160d22a8
Rita <ritapsantos@tecnico.ulisboa.pt>
2023-06-26 10:43:29 +0100

    convergence tests until t=2

---
+++ ScalarField/script-convergence.py
@@ -0,0 +1,32 @@
+import papermill as pm
+import sys
+
+
+print("Excuting nb for res = ", 3)
+pm.execute_notebook(
+    'Evolution_ScalarField.ipynb',
+    'Evolution_ScalarField.ipynb',
+    parameters = dict(m=3,A = 0.01)
+)
+
+
+"""
+for i in range(1, len(sys.argv)):
+    print('\nResolution:', sys.argv[i],'\n\n')
+    """
+
+""""for i in range(1,3+1):
+    print("Excuting nb for res = ", i)
+    pm.execute_notebook(
+        'Evolution_ScalarField.ipynb',
+        'Evolution_ScalarField.ipynb',
+        parameters = dict(m=i,A = 0.01)
+    )
+   
+""""
+
+    """pm.execute_notebook(
+    'Evolution_ScalarField.ipynb',
+    'Evolution_ScalarField.ipynb',
+    parameters = dict(m=int(sys.argv[i]))
+    )"""
