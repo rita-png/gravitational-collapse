c277da83a4692075a34f1a0007a50b44ab07e654
Rita <ritapsantos@tecnico.ulisboa.pt>
2023-06-26 11:00:26 +0100

    convergence tests until t=2

--- ScalarField/script-convergence.py
+++ ScalarField/script-convergence.py
@@ -13,9 +13,9 @@ pm.execute_notebook(
 """
 for i in range(1, len(sys.argv)):
     print('\nResolution:', sys.argv[i],'\n\n')
-    """
+"""
 
-""""for i in range(1,3+1):
+"""for i in range(1,3+1):
     print("Excuting nb for res = ", i)
     pm.execute_notebook(
         'Evolution_ScalarField.ipynb',
@@ -23,9 +23,9 @@ for i in range(1, len(sys.argv)):
         parameters = dict(m=i,A = 0.01)
     )
    
-""""
+"""
 
-    """pm.execute_notebook(
+"""pm.execute_notebook(
     'Evolution_ScalarField.ipynb',
     'Evolution_ScalarField.ipynb',
     parameters = dict(m=int(sys.argv[i]))
