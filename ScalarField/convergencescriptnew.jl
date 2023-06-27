29d4754b06d2b2c6e389813b8595b5eb3ac5493e
Rita <ritapsantos@tecnico.ulisboa.pt>
2023-06-27 18:42:25 +0100

    convergence tests

--- ScalarField/convergencescriptnew.jl
+++ ScalarField/convergencescriptnew.jl
@@ -6,7 +6,7 @@ m = res
 #A = 0.01#0.0492646484375#0.001#0.049375#5#0.0492645084166179#0.049264507293701174#0.049264508247375494#0.04925#0.0493#0.001#0.049375#0.07#0.05#0.1124921875#0.125
 run = 1
 
-global loggrid=false#true
+global loggrid=true#true
 global bisection=false
 global meshrefinement=false;
 
@@ -32,8 +32,8 @@ println("dx =", dx)
 println("dt =", dt)
 println("dx/dt =", dx/dt)
 
-global dir = "/home/ritapsantos/data/ritapsantos" #~/data/ritapsantos" #"/home/rita13santos/Desktop/MSc Thesis/Git/ScalarField/DATA" #
-#global dir = "/home/rita13santos/Desktop/MSc Thesis/Git/ScalarField/DATA"
+#global dir = "/home/ritapsantos/data/ritapsantos" #~/data/ritapsantos" #"/home/rita13santos/Desktop/MSc Thesis/Git/ScalarField/DATA" #
+global dir = "/home/rita13santos/Desktop/MSc Thesis/Git/ScalarField/DATA"
 
 using Printf
 include("./ScalarField.jl");
@@ -47,8 +47,6 @@ initX = range(round(ori-3.0*dx,digits=10), stop=Xf+3.0*dx, step=dx)
 #initX=create_range(ori-3.0*dx,Xf+3.0*dx,dx,N+6)
 
 L=length(initX);
-println("step size is  ", dx)
-println("the time step is ", dt)
 
 if loggrid==true
     """global originalX=initX
@@ -66,11 +64,6 @@ end;
 using Dierckx
 
 
-####
-#initm=Array{Float128}(undef, L)
-#initbeta=Array{Float128}(undef, L)
-#initpsi=Array{Float128}(undef, L)
-#initderpsi=Array{Float128}(undef, L)
 initm=zeros(L);
 initbeta=zeros(L);
 initpsi=zeros(L);
@@ -79,7 +72,7 @@ initderpsi=zeros(L);
 state_array=[initm initbeta initpsi initderpsi initX];
 
 #PSI
-r0=0.7#0.01#0.7#0.01#0.7#0.7#0.7#0.01#0.7#0.3
+r0=0.7
 sigma=0.3
 initpsi[4:L-3] = init_gaussian(initX1,r0,sigma,A)
 
@@ -103,8 +96,6 @@ state_array = ghost(state_array);
 
 
 using CSV, Tables, Printf
-#CSV.write(dir*"/time_step0.csv", Tables.table(transpose(Matrix(state_array))), writeheader=false)
-#CSV.write(dir*"/res$res/time_step0.csv", Tables.table(state_array), writeheader=false)
 
 global files=["m", "beta", "psi", "derpsi"]
 
@@ -118,7 +109,6 @@ monitor_ratio = zeros(L)
 CSV.write(dir*"/parameters.csv", Tables.table(evol_stats), writeheader=true, header=["criticality", "A", "sigma", "r0", "time", "explode", "run"])
 
 ginit=speed(initX,state_array[:,1],state_array[:,2])
-#println(update_dt(initX,state_array[:,1],state_array[:,2],dx,ginit)/dt)
 
 using Base.Threads
 Threads.nthreads()
@@ -131,11 +121,11 @@ Threads.nthreads()
 
 #global dt = 5e-5/2/2/2 #RES3
 if m==1
-    global dt=0.05#5e-5/2#/5
+    global dt=5e-5/2/5#0.005
 elseif m==2
-    global dt=0.05/2#5e-5/2/2#/5
+    global dt=5e-5/2/2/5#0.005/2
 else
-    global dt=0.05/2/2#5e-5/2/2/2#/5
+    global dt=5e-5/2/2/2/5#0.005/2/2
 end
 
 finaltime=2.0#1.3#2.0#1.3#1.6#2#1.3#0.08*2#0.005*10*5#1#0.9#0.8#0.04*5*4#length(T)-1
