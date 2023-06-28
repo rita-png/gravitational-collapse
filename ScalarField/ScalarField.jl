d808188f0d2aea6a613f539b4c99d41df81248b6
Rita <ritapsantos@tecnico.ulisboa.pt>
2023-06-28 23:54:50 +0100

    bisect 10k even

--- ScalarField/ScalarField.jl
+++ ScalarField/ScalarField.jl
@@ -650,9 +650,9 @@ function timeevolution(state_array,finaltime,run)#(state_array,finaltime,dir,run
         
         #update time increment
 
-        """if criticality!=true#||dt>0.00000001
+        if criticality!=true#||dt>0.00000001
             global dt = update_dt(initX,state_array[:,1],state_array[:,2],dt,ginit)      
-        end"""
+        end
         t = t + dt
         if iter%500==0
             println("\n\niteration ", iter, " dt is ", dt, ", t=", t, " speed is ", speed(initX, state_array[:,1], state_array[:,2]), ", dx/dt=", dx/dt)
@@ -680,8 +680,8 @@ function timeevolution(state_array,finaltime,run)#(state_array,finaltime,dir,run
         
 
         run=int(run)
-        if iter%500==0
-        #if (iter%500==0&&t>0.3)||(t>0.85&&iter%10==0)
+        #if iter%500==0
+        if (iter%500==0&&t>0.3)||(t>0.85&&iter%10==0)
             print_muninn(files, t, state_array[:,1:5],res,"a")
 
         end
