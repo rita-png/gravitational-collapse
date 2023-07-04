98619fe32f1c17c78660e9baa5bc089e6d76871e
Rita <ritapsantos@tecnico.ulisboa.pt>
2023-07-05 00:15:21 +0100

    conv test 4th order loggrid

--- ScalarField/convergencescriptnew.jl
+++ ScalarField/convergencescriptnew.jl
@@ -2,41 +2,44 @@
 res = trunc(Int, ARGS[2])
 println("running for resolution ", res, " N1 = 200", ", A = ", A)"""
 
-m = 2
-A = 0.01#0.0492646484375#0.001#0.049375#5#0.0492645084166179#0.049264507293701174#0.049264508247375494#0.04925#0.0493#0.001#0.049375#0.07#0.05#0.1124921875#0.125
+m = 3
+A = 0.1#0.0492646484375#0.001#0.049375#5#0.0492645084166179#0.049264507293701174#0.049264508247375494#0.04925#0.0493#0.001#0.049375#0.07#0.05#0.1124921875#0.125
 run = 1
 
-global loggrid=true#true
-global bisection=false
-global meshrefinement=false;
 
-using Printf
+global compactified=true
+global loggrid=true
+
+global bisection=false
+
+using Quadmath, Printf
 
 res=m;
 N=2.0^m*200.0/2.0
-Xf=1.0
 
-dx=Xf/N
+if compactified==true
+    Xf=1.0
+else
+    Xf=10.0#Float128(1.0);
+end
+
+dx=Xf/N#Float128(Xf/N);
 if loggrid==false
-    dt=0.5*round(dx,digits=10)
+    dt=0.5*round(dx,digits=10)#0.5*dx#round(dx,digits=10);#dx
 else
     dt=0.1*round(dx,digits=10)
 end
-Nt=N
+Nt=2.0^m*200.0/2.0
 Tf=Nt*dt; #final time
 #print(Tf)
 
 print(N)
 
-println("dx =", dx)
-println("dt =", dt)
-println("dx/dt =", dx/dt)
-
-global dir = "/home/ritapsantos/data/ritapsantos" #~/data/ritapsantos" #"/home/rita13santos/Desktop/MSc Thesis/Git/ScalarField/DATA" #
-#global dir = "/home/rita13santos/Desktop/MSc Thesis/Git/ScalarField/DATA"
+global dir = "/home/ritapsantos/data/ritapsantos"#global dir = "/home/rita13santos/Desktop/MSc Thesis/Git/ScalarField/DATA"
 
 using Printf
 include("./ScalarField.jl");
+#include("/home/rita13santos/Desktop/MSc Thesis/Git/ScalarField/myspline.jl");
 
 ori=0.0#Float128(0.0)#0.0;
 initX1 = nothing
@@ -47,6 +50,8 @@ initX = range(round(ori-3.0*dx,digits=10), stop=Xf+3.0*dx, step=dx)
 #initX=create_range(ori-3.0*dx,Xf+3.0*dx,dx,N+6)
 
 L=length(initX);
+println("step size is  ", dx)
+println("the time step is ", dt)
 
 if loggrid==true
     global originalX=initX
@@ -54,33 +59,35 @@ if loggrid==true
     initX1=xtilde
     initX=collect(initX)
     initX[4:L-3]=xtilde
-    """global originalX=initX
-    xtilde=gridfunc(initX1)
-    initX1=xtilde
-    initX=collect(initX)
-    initX[4:L-4]=xtilde[1:length(xtilde)-1]"""
 end;
+
+
 
 using Dierckx
 
 
-initm=zeros(L);
-initbeta=zeros(L);
-initpsi=zeros(L);
-initderpsi=zeros(L);
+####
+
+initm=zeros(L)
+initbeta=zeros(L)
+initpsi=zeros(L)
+initderpsi=zeros(L)
+#initm=Array{Float128}(undef, L)
+#initbeta=Array{Float128}(undef, L)
+#initpsi=Array{Float128}(undef, L)
+#initderpsi=Array{Float128}(undef, L)
 
 state_array=[initm initbeta initpsi initderpsi initX];
 
 #PSI
-r0=0.7
-sigma=0.3
-initpsi[4:L-3] = init_gaussian(initX1,r0,sigma,A)
+r0=0.7#Float128(0.7)#0.01#0.7#0.01#0.7#0.7#0.7#0.01#0.7#0.3
+sigma=0.3#Float128(0.3)
 
-state_array[:,3] = initpsi
-state_array = ghost(state_array)
 
 #PSI,X FROM PSI
 initderpsi[4:L-3] = init_gaussian_der(initX1,r0,sigma,A)
+
+
 state_array[:,4] = initderpsi
 state_array=ghost(state_array)
 
@@ -88,14 +95,28 @@ state_array=ghost(state_array)
 
 derpsi_func = Spline1D(initX[4:L-3], state_array[4:L-3,4],  k=4);
 
-y0=[0 0 0]
+#derpsi_func(state_array[:,5]); #doesnt output the quad precision
+#coef=splinethree(state_array[4:L-3,5],state_array[4:L-3,4],state_array[4:L-3,5]);
+
+#evalInterval(Float128.([0.1]),initX,coef,3);
+
+y0=Float128.([0.0 0.0 0.0])
 
 state_array[4:L-3,1:3] = n_rk4wrapper(RHS,y0,initX[4:L-3],0,derpsi_func,state_array[:,:]);
 
 state_array = ghost(state_array);
 
 
-using CSV, Tables, Printf
+
+
+
+
+
+using Plots #showing initial data and the initial status of the horizon finder
+plot((initX[4:L-3]), state_array[4:L-3,1:4], xaxis="r",title="Initial Data", labels=["m" "beta" "psi" "psi,x"],xlim=[0,1])
+
+using CSV, Tables
+#CSV.write(dir*"/res$res/time_step0.csv", Tables.table(state_array), writeheader=false)
 
 global files=["m", "beta", "psi", "derpsi"]
 
@@ -105,30 +126,25 @@ time=0.0
 criticality=0.0
 explode=0.0
 evol_stats = [criticality A sigma r0 time explode run]
-monitor_ratio = zeros(L)
-CSV.write(dir*"/parameters.csv", Tables.table(evol_stats), writeheader=true, header=["criticality", "A", "sigma", "r0", "time", "explode", "run"])
+global monitor_ratio = zeros(L);
+#CSV.write(dir*"/parameters.csv", Tables.table(evol_stats), writeheader=true, header=["criticality", "A", "sigma", "r0", "time", "explode", "run"])
 
 ginit=speed(initX,state_array[:,1],state_array[:,2])
+println(update_dt(initX,state_array[:,1],state_array[:,2],dx,ginit)/dt)
 
 using Base.Threads
 Threads.nthreads()
 
-
-
-
-
-
-
-#global dt = 5e-5/2/2/2 #RES3
 if m==1
-    global dt=5e-5/2#0.005
+    global dt=2e-5
 elseif m==2
-    global dt=5e-5/2/2#0.005/2
+    global dt=1e-5
 else
-    global dt=5e-5/2/2/2#0.005/2/2
+    global dt=1e-5/2
 end
 
-finaltime=2.0#1.3#2.0#1.3#1.6#2#1.3#0.08*2#0.005*10*5#1#0.9#0.8#0.04*5*4#length(T)-1
-evol_stats, T_interp = timeevolution(state_array,finaltime,run);#timeevolution(state_array,finaltime,dir*"/res$res",run,auxstate_array);
+finaltime=5.0
+evol_stats, T_interp = timeevolution(state_array,finaltime,run);
 #CSV.write(dir*"/timearray$res.csv", Tables.table(T_interp), writeheader=false);
+#CSV.write(dir*"/parameters.csv", Tables.table(evol_stats), writeheader=false);
 
