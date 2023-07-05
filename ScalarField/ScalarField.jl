13e1175a028dcbd551f442a170be5af0835833fc
Rita <ritapsantos@tecnico.ulisboa.pt>
2023-07-05 11:20:52 +0100

    bisec with 4th even

--- ScalarField/ScalarField.jl
+++ ScalarField/ScalarField.jl
@@ -750,7 +750,7 @@ function timeevolution(state_array,finaltime,run)#(state_array,finaltime,dir,run
             global dt = update_dt(initX,state_array[:,1],state_array[:,2],dt,ginit)      
         end"""
         t = t + dt
-        if iter%500==0
+        if iter%100==0
             println("\n\niteration ", iter, " dt is ", dt, ", t=", t, " speed is ", speed(initX, state_array[:,1], state_array[:,2]), ", dx/dt=", dx/dt)
         end
         #println("\n\niteration ", iter, " dt is ", dt, ", t=", t, " speed is ", speed(initX, state_array[:,1], state_array[:,2]), ", dx/dt=", dx/dt)
@@ -777,8 +777,8 @@ function timeevolution(state_array,finaltime,run)#(state_array,finaltime,dir,run
 
         run=int(run)
 
-        if iter%500==0
-        #if (iter%50==0&&t>0.5)||(t>1.5&&iter%10==0)
+        #if iter%500==0
+        if (iter%100==0&&t>0.5)||(t>1.5&&iter%5==0)
             print_muninn(files, t, state_array[:,1:5],res,"a")
 
         end
