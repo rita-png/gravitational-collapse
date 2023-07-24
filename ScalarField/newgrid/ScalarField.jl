4b74972a473bec53845f1dc902e7cc57fe22ba28
Rita <ritapsantos@tecnico.ulisboa.pt>
2023-07-24 13:43:33 +0100

    bisec 4th uneven

--- ScalarField/newgrid/ScalarField.jl
+++ ScalarField/newgrid/ScalarField.jl
@@ -920,7 +920,7 @@ function timeevolution(state_array,finaltime,run)
 
         t = t + dt
         if ((iter%2000==0)&&(bisection==true))||((iter%500==0)&&(bisection==false))
-            println("\n\niteration ", iter, " dt is ", dt, ", t=", t, " speed is ", speed(initX, state_array[:,1], state_array[:,2]), ", dx/dt=", dx/dt)
+            println("\n\niteration ", iter, " dt is ", dt, ", t=", t, " speed is ", speed(initX, state_array[:,1], state_array[:,2]), ", dx/dt=", dx/dt, " monitor ratio is ", maximum(monitor_ratio))
         end
         #println("\n\niteration ", iter, " dt is ", dt, ", t=", t, " speed is ", speed(initX, state_array[:,1], state_array[:,2]), ", dx/dt=", dx/dt)
 
--- ScalarField/newgrid/bisectionsearch.jl
+++ ScalarField/newgrid/bisectionsearch.jl
@@ -12,19 +12,18 @@ include("./ScalarField.jl");
 #### CONFIG ####
 
 #2nd
-global low_bound = 0.15109375
-global high_bound = 0.15132812499999998#0.1515625
+#global low_bound = 0.15109375
+#global high_bound = 0.15132812499999998#0.1515625
 
 ## 4th
-#global low_bound = 0.15151855468749997
-#global high_bound = 0.1515625
-
+global low_bound = 0.15151855468749997
+global high_bound = 0.15154052734375#0.1515625
 
 global bisection = true
 global loggrid = true
 global compactified = true
 global zeroformat = true
-global twod = true
+global twod = false
 
 #global dir = "/home/ritapsantos/data/ritapsantos/4theven"
 
