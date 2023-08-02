afd516ddcb3e68c6f34c7e11a41a3146e2afa115
Rita <ritapsantos@tecnico.ulisboa.pt>
2023-08-02 18:43:40 +0100

    bisec 2nduneven, now diss4

--- ScalarField/newgrid/ScalarField.jl
+++ ScalarField/newgrid/ScalarField.jl
@@ -481,7 +481,7 @@ function dissipation(y,i,eps)
     if twod==true
         return dissipation6(y,i,0.0065)
     else
-        return dissipation6(y,i,eps) 
+        return dissipation4(y,i,eps) 
     end
 end
 
