d3961c9fdce8c6137d433083bff4e8c32d596663
Rita <ritapsantos@tecnico.ulisboa.pt>
2023-09-06 12:04:24 +0100

    conv test 1600 points uneven

--- ScalarField/newgrid/executeconv.jl
+++ ScalarField/newgrid/executeconv.jl
@@ -1,9 +1,12 @@
-#resol=[1,2,3]
-
-resol=[4]
+"""resol=[1,2,3]
 
 for ress in resol
     A=0.05#0.1
     global ARGS = [A,ress]
     include("./convergencescriptnew.jl");
-end
+end"""
+
+ress=4
+A=0.05#0.1
+global ARGS = [A,ress]
+include("./convergencescriptnew.jl");
