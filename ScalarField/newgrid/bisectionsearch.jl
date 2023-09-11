f6737ff60cfdd084b12966a05ca21ae6fcaf8493
Rita <ritapsantos@tecnico.ulisboa.pt>
2023-09-11 18:34:23 +0100

    2000 uneven bisec

--- ScalarField/newgrid/bisectionsearch.jl
+++ ScalarField/newgrid/bisectionsearch.jl
@@ -21,17 +21,17 @@ global twod = true
 
 #global dir = "/home/ritapsantos/data/ritapsantos/4theven"
 
-#N=800.0
-#global dir = "/home/ritapsantos/data/ritapsantos/2nduneven"
-#global low_bound = 0.115
-#global high_bound = 0.135
+N=2000.0
+global dir = "/home/ritapsantos/data/ritapsantos/2nduneven"
+global low_bound = 0.115
+global high_bound = 0.135
 
 ##OR##
 
-N=1200.0
-global dir = "/home/ritapsantos/data/ritapsantos"
-global low_bound = 0.115
-global high_bound = 0.135
+#N=1200.0
+#global dir = "/home/ritapsantos/data/ritapsantos"
+#global low_bound = 0.115
+#global high_bound = 0.135
 
 #### CONFIG ####
 
