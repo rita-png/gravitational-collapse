ecedf64f62803bd8376b08c0faacd037330d2d6a
Rita <ritapsantos@tecnico.ulisboa.pt>
2023-09-20 15:59:24 +0100

    grid 2, 2000 points

--- ScalarField/newgrid/bisectionsearch.jl
+++ ScalarField/newgrid/bisectionsearch.jl
@@ -34,17 +34,17 @@ global twod = true
 
 ##OR##
 
-N=1200.0 #grid 2, tmux 2
-global dir = "/home/ritapsantos/data/ritapsantos"
-global low_bound = 0.115#0.125
-global high_bound = 0.12500000000931322#0.135
+#N=1200.0 #grid 2, tmux 2
+#global dir = "/home/ritapsantos/data/ritapsantos"
+#global low_bound = 0.115#0.125
+#global high_bound = 0.12500000000931322#0.135
 
 ##OR##
 
-#N=1200.0 #grid 1
-#global dir = "/home/ritapsantos/data/ritapsantos/new2nduneven"
-#global low_bound = 0.125966796875
-#global high_bound = 0.1259765625
+N=2000.0 #grid 2
+global dir = "/home/ritapsantos/data/ritapsantos/new2nduneven"
+global low_bound = 0.12
+global high_bound = 0.13
 
 
 #### CONFIG ####
