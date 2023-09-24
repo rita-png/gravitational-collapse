a264bc730f0cc80e1e4a215bc2ea8a37d231a2cb
Rita <ritapsantos@tecnico.ulisboa.pt>
2023-09-24 23:57:25 +0100

    2nd even 1600 points

--- ScalarField/convergencescriptnew.jl
+++ ScalarField/convergencescriptnew.jl
@@ -14,7 +14,7 @@ global bisection=false
 global twod=true
 
 res=m;
-N=2.0^m*200.0/2.0
+N=2.0^m*1600.0/2.0
 println("running for resolution ", res, ", N = ", N, ", A = ", A)
 if compactified==true
     Xf=1.0
@@ -138,9 +138,9 @@ using Base.Threads
 Threads.nthreads()
 
 if m==1
-    global dt=2e-5 #N=200
+    #global dt=2e-5 #N=200
     #global dt=5e-5 #N=150
-    #global dt=5e-5/2/2/2/2
+    global dt=5e-5/2/2/2/2
 elseif m==2
     global dt=1e-5
     #global dt=5e-5/2
