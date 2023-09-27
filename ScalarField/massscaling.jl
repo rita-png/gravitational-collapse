767781c0b64b2e633c790705740522aa9ee547b4
Rita <ritapsantos@tecnico.ulisboa.pt>
2023-09-27 13:25:28 +0100

    massscaling new script

--- ScalarField/massscaling.jl
+++ ScalarField/massscaling.jl
@@ -27,7 +27,7 @@ global twod = false
 
 A_critic = 0.12465049029170887
 
-exponents = collect(-30:0.5:-5)
+exponents = collect(-30:0.5:-7)
 
 global run = 1
 global runmax = length(exponents)
@@ -40,28 +40,14 @@ while(run <= runmax)
     println("\nBisection search run ##", run, "; A = ", A," N = ", N,", exponent = ", exponents[run],"\n")
 
     global ARGS = [A,run,N]
-    """include("./Evolution_ScalarField.jl");
+    include("./Evolution_ScalarField.jl");
     if loggrid==true
         df = CSV.read(dir*"/bisectionsearch/muninnDATA/uneven/parameters.csv", DataFrame)
     else
         df = CSV.read(dir*"/bisectionsearch/muninnDATA/even/parameters.csv", DataFrame)
     end
 
-    if (df[run+1, :Column1]) == 0.0 #:criticality
-        println("\nNon critical!")
-        push!(plt_A_non_crit, A)
-        push!(plt_x1, run)
-    else
-        println("\nCritical!")
-        push!(plt_A_crit, A)
-        push!(plt_x2, run)
-    end
-    println("\nA = ",df[run+1, :Column2], " sigma = ", df[run+1, :Column3], " r0 = ", df[run+1, :Column4], " Final time = ", df[run+1, :Column5], " explode = ", df[run+1, :Column6])
-
-    #println(df[1, :explode])
-    if (df[run+1, :Column6]) == 1.0 #:explode
-        println("Found a NaN")
-    end"""
+    println("\nA = ",df[run+1, :Column2], " sigma = ", df[run+1, :Column3], " r0 = ", df[run+1, :Column4], " Final time = ", df[run+1, :Column5], " explode = ", df[run+1, :Column6], " black hole mass = ", df[run+1, :Column7])
     
     
     global run = run + 1
