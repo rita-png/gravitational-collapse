0e243d2c5c997109604052100f03d1ccd6a3aeb9
Rita <ritapsantos@tecnico.ulisboa.pt>
2023-10-11 11:08:30 +0100

    before changing scripts to massscaling

--- ScalarField/Evolution_ScalarField_Diff_Families.jl
+++ ScalarField/Evolution_ScalarField_Diff_Families.jl
@@ -94,7 +94,8 @@ time=0
 criticality=0.0
 explode=0.0
 critical_stop=0
-evol_stats = [criticality A sigma r0 time explode run]
+bondimass=0
+evol_stats = [criticality A sigma r0 time explode run bondimass]
 monitor_ratio = zeros(L)
 
 run=int(run)
