433d3fc795d1ed2fef0c1f29d1447ed5a32fd6ff
Rita <ritapsantos@tecnico.ulisboa.pt>
2023-10-28 20:45:57 +0100

    bisec 1 balta

--- TMYangMills/Evolution_TMTHREEYangMills.jl
+++ TMYangMills/Evolution_TMTHREEYangMills.jl
@@ -79,6 +79,7 @@ global files=["m", "beta", "xi", "derxi", "derderxi"]
 
 derderxi=Der_arrayLOP(state_array,4,initX) .* (initX .- 1) .^ 2
 
+global res=1
 if zeroformat==true
     zero_print_muninn(files, 0, [state_array[:,1:4] derderxi],res,"w", initX)
 else
