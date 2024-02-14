2b9bff4b34b07f11662e5053cfd81de1d3f90235
Rita <ritapsantos@tecnico.ulisboa.pt>
2024-02-14 16:59:02 +0100

    bisec2

--- TMYangMills/bisectionsearch.jl
+++ TMYangMills/bisectionsearch.jl
@@ -20,7 +20,7 @@ global source=false
 #global N=2000.0
 #global dir = "/home/ritapsantos/data/ritapsantos/YangMillsbisec1"
 #global high_bound=0.0885
-#global low_bound=1.1#0.08
+#global low_bound=0.08
 
 
 ####bisec2
@@ -28,7 +28,7 @@ global source=false
 global N=4000.0
 global dir = "/home/ritapsantos/data/ritapsantos/YangMillsbisec2"
 global high_bound=0.0885
-global low_bound=1.1#0.08
+global low_bound=0.08
 
 ####bisec3
 
@@ -42,7 +42,7 @@ global low_bound=1.1#0.08
 #global N=2000.0
 #global dir = "/home/ritapsantos/data/ritapsantos/YangMillsbisec3"
 #global low_bound = 0.0885
-#global high_bound = 1.1#0.9
+#global high_bound = 0.08
 #
 
 
